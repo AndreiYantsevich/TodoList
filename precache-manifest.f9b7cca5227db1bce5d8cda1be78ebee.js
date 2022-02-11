415615f7583c08473f973c584de231b3abc8476c
Andrei <yantsevich92@gmail.com>
2022-02-12 01:31:16 +0300

    Updates

---
+++ precache-manifest.f9b7cca5227db1bce5d8cda1be78ebee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b21660e0bfa1bf098b885a46a77123",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "e1acb70bc2f98850b4b4",
+    "url": "/TodoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "01369b4d598145abdc61",
+    "url": "/TodoList/static/js/2.57dc9a5c.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.57dc9a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1acb70bc2f98850b4b4",
+    "url": "/TodoList/static/js/main.6b8fc208.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
